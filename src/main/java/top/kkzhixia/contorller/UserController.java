793b3ddb23aa3388198e95fd8b8d801e7c4c43dc
liuchunpeng <liu_chunpeng@qq.com>
2017-10-30 18:04:10 +0800

    提交

--- src/main/java/top/kkzhixia/contorller/UserController.java
+++ src/main/java/top/kkzhixia/contorller/UserController.java
@@ -1,5 +1,15 @@
 package top.kkzhixia.contorller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class UserController {
 
+	@RequestMapping("/getuser")
+	String getUser(){
+		
+		return "[{name:张三},{avg:18},{sex:男}]";
+	}
 }
